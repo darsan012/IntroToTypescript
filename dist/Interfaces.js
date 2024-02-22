17ee43505132226a9d8082ad3715b9db7b047ee3
darsan012 <dsrsangautam@gmail.com>
2024-02-21 23:51:15 -0500

    extended interface and differenciated between type and interface

--- dist/Interfaces.js
+++ dist/Interfaces.js
@@ -8,7 +8,23 @@ const me = {
     spend(amount) {
         console.log(amount);
         return amount;
-    }
+    },
+    height: 6
 };
 console.log(me);
+let asianPerson = {
+    name: 'Joma',
+    age: 25,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log(amount);
+        return amount;
+    },
+    height: 6,
+    skinType: 'dark',
+    race: 'asian'
+};
+console.log(asianPerson);
 //# sourceMappingURL=Interfaces.js.map
--- src/Interfaces.ts
+++ src/Interfaces.ts
@@ -6,6 +6,14 @@ interface isPerson {
     spend(a: number): number;
 }
 
+// interface and types are similar with subtle differences
+// use types when creating shape of values
+// you can extend interfaces
+interface isPerson {
+    height: number;
+}
+
+// now the interface has height variables aswell
 const me: isPerson = {
     name: 'Darshan',
     age: 25,
@@ -15,7 +23,30 @@ const me: isPerson = {
     spend(amount: number): number {
         console.log(amount);
         return amount;
-    }
+    },
+    height: 6
 }
 
 console.log(me);
+
+interface isExtendedPerson extends isPerson {
+    skinType: string;
+    race: string;
+} 
+
+let asianPerson: isExtendedPerson = {
+    name: 'Joma',
+    age: 25,
+    speak(text: string): void {
+        console.log(text);
+    },
+    spend(amount: number): number {
+        console.log(amount);
+        return amount;
+    },
+    height: 6,
+    skinType: 'dark',
+    race: 'asian'
+}
+
+console.log(asianPerson);
